5fc62ff3f1acf90063e176259f343d0f9c374b13
wezer-pixel <jerryjuma104@gmail.com>
2023-09-22 06:45:19 -0400

    select cities of a state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,12 +9,11 @@ if __name__ == "__main__":
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("""
-        SELECT cities.name 
-        FROM cities
-        INNER JOIN states ON citites.states_id - states.id
+        SELECT cities.name FROM cities
+        JOIN states ON cities.state_id = states.id
         WHERE states.name = %s
-        ORDER BY cities.id ASC""", (argv[4],))
+        ORDER BY cities.id ASC
+        """, (argv[4], ))
     print(", ".join(map(lambda x: x[0], cur.fetchall())))
     cur.close()
     conn.close()
-
